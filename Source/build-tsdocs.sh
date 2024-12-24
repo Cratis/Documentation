b61d61dd16ae7b10f1d7d60b26243284db1f4dcf
Einar Ingebrigtsen <einari@me.com>
2024-12-24 10:11:09 +0100

    Adding Fundamentals

--- Source/build-tsdocs.sh
+++ Source/build-tsdocs.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 echo "Building TypeScript documentation..."
 
+restore_paths=(
+    "../ApplicationModel"
+    "../Fundamentals"
+)
+
 # Define source and target paths
 source_paths=(
     "../ApplicationModel/Source/JavaScript/Applications"
     "../ApplicationModel/Source/JavaScript/Applications.React"
     "../ApplicationModel/Source/JavaScript/Applications.React.MVVM"
     "../ApplicationModel/Source/JavaScript/Applications.Vite"
+    "../Fundamentals/Source/JavaScript"
 )
 
 target_paths=(
@@ -14,18 +20,30 @@ target_paths=(
     "./api/applicationmodel/javascript/applications.react"
     "./api/applicationmodel/javascript/applications.react.mvvm"
     "./api/applicationmodel/javascript/applications.vite"
+    "./api/fundamentals/javascript"
 )
 
-# Loop through the arrays
+for i in "${!restore_paths[@]}"; do
+    restore_path="${restore_paths[$i]}"
+    echo "Restoring packages in $restore_path..."
+    pushd .
+    cd "$restore_path"
+    yarn install
+    popd
+done
+
 for i in "${!source_paths[@]}"; do
     source_path="${source_paths[$i]}"
     target_path="${target_paths[$i]}"
 
+    echo "Building documentation for $source_path"
+
     pushd .
     cd "$source_path"
+    echo "Current directory: $(pwd)"
     yarn build
     popd
     yarn typedoc --plugin typedoc-plugin-markdown --hidePageHeader true --outputFileStrategy modules -out "$target_path" --tsconfig "$source_path/tsconfig.json" "$source_path/**/*.ts" --exclude "$source_path/dist/**/*" --exclude "$source_path/**/for_*/**/*"
 done
 
-yarn fixts
+# yarn fixts
