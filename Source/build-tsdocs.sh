f7b8daee45912572f3df200fed6116e10006f7a3
Einar <einari@me.com>
2026-02-05 16:14:39 +0100

    Fixing links

--- Source/build-tsdocs.sh
+++ Source/build-tsdocs.sh
@@ -23,6 +23,14 @@ target_paths=(
     "./api/fundamentals/javascript"
 )
 
+doc_names=(
+    "Arc"
+    "Arc.React"
+    "Arc.React.MVVM"
+    "Arc.Vite"
+    "Fundamentals"
+)
+
 for i in "${!restore_paths[@]}"; do
     restore_path="${restore_paths[$i]}"
     echo "Restoring packages in $restore_path..."
@@ -35,6 +43,7 @@ done
 for i in "${!source_paths[@]}"; do
     source_path="${source_paths[$i]}"
     target_path="${target_paths[$i]}"
+    doc_name="${doc_names[$i]}"
 
     echo "Building documentation for $source_path"
 
@@ -43,7 +52,7 @@ for i in "${!source_paths[@]}"; do
     echo "Current directory: $(pwd)"
     yarn build
     popd
-    yarn typedoc --plugin typedoc-plugin-markdown --hidePageHeader true --outputFileStrategy modules -out "$target_path" --tsconfig "$source_path/tsconfig.json" "$source_path/**/*.ts" --exclude "$source_path/dist/**/*" --exclude "$source_path/**/for_*/**/*" --exclude "$source_path/**/global.d.ts"
+    yarn typedoc --plugin typedoc-plugin-markdown --name "$doc_name" --hidePageHeader true --outputFileStrategy modules -out "$target_path" --tsconfig "$source_path/tsconfig.json" "$source_path/**/*.ts" --exclude "$source_path/dist/**/*" --exclude "$source_path/**/for_*/**/*" --exclude "$source_path/**/global.d.ts"
 done
 
 yarn fixts
